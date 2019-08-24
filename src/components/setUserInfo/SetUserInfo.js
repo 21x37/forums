d8ba816dd47d58bde0ac0021d0ec24dd6a4b482e
Tyler Mok <tylermok@outlook.com>
2019-08-24 13:57:52 -0700

    style set user info page

--- src/components/setUserInfo/SetUserInfo.js
+++ src/components/setUserInfo/SetUserInfo.js
@@ -8,6 +8,9 @@ import { startLogout } from '../../actions/auth';
 class SetUserInfo extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            fileUploaded: false
+        }
         
         this.onUserNameChange = this.onUserNameChange.bind(this);
         this.onFileUploaded = this.onFileUploaded.bind(this);
@@ -23,7 +26,7 @@ class SetUserInfo extends React.Component {
     }
     onFileUploaded(e) {
         const profilePicture = e.target.files[0]
-        this.setState({ profilePicture });
+        this.setState({ profilePicture, fileUploaded: true });
     }
     async onSubmit() {
         // TODO: Refactor getEmail
@@ -43,15 +46,20 @@ class SetUserInfo extends React.Component {
     }
     render() {
         return (
-            <div>
-                <h2>Set a username and profile picture to cotinue.</h2>
-                <button onClick={this.props.startLogout}>Logout</button>
-                <p>Username</p>
-                <input name='username' placeholder='Shanaynay104' type='text' onChange={this.onUserNameChange} />
-                <p>Profile Picture</p>
-                <input type='file' onChange={this.onFileUploaded}/>
-                {/* <img src={this.state.previewFile}/>  */}
-                <button onClick={this.onSubmit}>Save</button>
+            <div className='set__user__info__container'>
+                <div className='set__user__info__wrapper'>
+                    <h1 className='logIn__title'>Tattle</h1>
+                    <h2 className='set__user__info'>Set a username and profile picture to cotinue</h2>
+                    <button className='logOut__button' onClick={this.props.startLogout}>Logout</button>
+                    <button className={`save__button ${this.state.username && this.state.profilePicture ? 'formFilledOut' : ''}`} onClick={this.onSubmit}>Save</button>
+                    <p className='set__username__text'>Username</p>
+                    <input className='set__username__input' name='username' placeholder='Shanaynay104' type='text' onChange={this.onUserNameChange} />
+                    <div className='set__profilePicture__wrapper'>
+                        <p className='set__profilePicture__text'>Profile Picture</p>
+                        <label for='profilePicture__upload'><div className={`set__profilePicture__button ${this.state.fileUploaded ? 'profilePicture__uploaded' : ''}`}>{this.state.fileUploaded ? 'Uploaded' : 'Upload'}</div></label>
+                        <input style={{display: 'none'}} id='profilePicture__upload' className='set__profilePicture__input' type='file' onChange={this.onFileUploaded}/>
+                    </div>
+                </div>
             </div>
         );
 
